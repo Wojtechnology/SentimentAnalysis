60b9658ead20dc315e17095183c77054e2e071a6
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-07-01 22:40:07 -0400

    Committing some throwaway work just in case

---
+++ src/main/scala/com/wojtechnology/sentiment/actions/ActionState.scala
@@ -0,0 +1,8 @@
+package com.wojtechnology.sentiment.actions
+
+/**
+  * Created by wojtekswiderski on 2016-07-01.
+  */
+class ActionState {
+
+}
---
+++ src/test/scala/com/wojtechnology/sentiment/parse/ArgumentParserSuite.scala
@@ -0,0 +1,8 @@
+package com.wojtechnology.sentiment.parse
+
+/**
+  * Created by wojtekswiderski on 2016-07-01.
+  */
+class ArgumentParserSuite {
+
+}
