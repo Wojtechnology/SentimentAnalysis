b38ad94ad6ad4291c7490a8ded430b575eef0f5c
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-07-01 23:57:41 -0400

    working spark program

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test"
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test",
+  "org.apache.spark" %% "spark-core" % "1.6.2"
+)
--- src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
+++ src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
@@ -1,10 +1,22 @@
 package com.wojtechnology.sentiment
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /** Main object for SentimentAnalysis tool */
 object SentimentAnalysis {
 
   /** Main function for program */
-  def main(argsArray: Array[String]): Unit = {
-    println("I'll analyze your sentiment yo")
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      println("Did not provide a filename")
+    } else {
+      val fileName = args.head
+      val conf = new SparkConf().setAppName("SentimentAnalysis")
+      val sc = new SparkContext(conf)
+      val logData = sc.textFile(fileName, 2).cache()
+      val numAs = logData.filter(line => line.contains("a")).count()
+      val numBs = logData.filter(line => line.contains("b")).count()
+      println(s"Lines with a: $numAs, Lines with b: $numBs")
+    }
   }
 }
