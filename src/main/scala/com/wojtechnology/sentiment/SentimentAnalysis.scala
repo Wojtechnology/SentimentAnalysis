a7f6b267696ba00de8bf14974f057b6b4a3cb3f7
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-07-01 22:45:34 -0400

    removed tests also

--- src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
+++ src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
@@ -1,16 +1,10 @@
 package com.wojtechnology.sentiment
 
-import com.wojtechnology.sentiment.actions.ActionList
-import com.wojtechnology.sentiment.parse.ArgumentParser
-
 /** Main object for SentimentAnalysis tool */
 object SentimentAnalysis {
 
   /** Main function for program */
   def main(argsArray: Array[String]): Unit = {
-    val args: Seq[String] = argsArray.slice(1, argsArray.size)
-    val actions = ArgumentParser.parse(args)
-    val actionList = new ActionList(actions)
-    actionList.run()
+    println("I'll analyze your sentiment yo")
   }
 }
