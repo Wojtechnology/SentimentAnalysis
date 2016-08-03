beca0a9ee23987638900191bbde88f1d766281f8
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-08-03 19:06:59 -0400

    Cleanup main

--- src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
+++ src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
@@ -1,6 +1,7 @@
 package com.wojtechnology.sentiment
 
 import com.wojtechnology.sentiment.parse.{CSVParser, ColumnsFilter}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /** Main object for SentimentAnalysis tool */
@@ -14,27 +15,35 @@ object SentimentAnalysis {
       val fileName = args.head
       val sc = initSpark("SentimentAnalysis")
 
-      val parser = new CSVParser with ColumnsFilter
-
-      val rawData = sc.textFile(fileName)
-      val parsed = parser.parse(rawData)
-      val filtered = parser.filter(parsed, Seq(0,3,4,5))
-      val hasSmiley = filtered.filter(_(3).contains(":("))
-      val countSmiley = hasSmiley.count()
-
-      println(countSmiley)
-      hasSmiley.foreach(println)
-      // filtered.take(10).foreach(println)
+      countTokens(sc.textFile(fileName), ":(")
     }
   }
 
   /**
     * Init SparkContext with given app name
-    * @param appName
+    * @param appName Name of Spark Application
     * @return SparkContext for use in program
     */
   def initSpark(appName: String): SparkContext = {
     val conf = new SparkConf().setAppName(appName)
     new SparkContext(conf)
+  }
+
+  /**
+    * Counts number of rows in data containing token and print first 10
+    * @param rawData Raw file data
+    * @param token token to count
+    * @return Unit
+    */
+  def countTokens(rawData: RDD[String], token: String) = {
+    val parser = new CSVParser with ColumnsFilter
+
+    val parsed = parser.parse(rawData)
+    val filtered = parser.filter(parsed, Seq(5))
+    val hasSmiley = filtered.filter(_.head.contains(token))
+    val countSmiley = hasSmiley.count()
+
+    println(s"Found $countSmiley smileys!")
+    hasSmiley.foreach(println)
   }
 }
