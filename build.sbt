7af58ca6de60d57c7dc93cc32cbca53d8ed9ea71
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-08-04 10:58:27 -0400

    Machine learning stuff

--- build.sbt
+++ build.sbt
@@ -2,10 +2,11 @@ name := "SentimentAnalysis"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.10.6"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test",
+  "org.scalatest" % "scalatest_2.10" % "3.0.0" % "test",
   "org.apache.spark" %% "spark-core" % "1.6.2",
+  "org.apache.spark" %% "spark-mllib" % "1.6.2",
   "com.holdenkarau" %% "spark-testing-base" % "1.6.1_0.3.3"
 )
--- src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
+++ src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
@@ -1,7 +1,11 @@
 package com.wojtechnology.sentiment
 
-import com.wojtechnology.sentiment.parse.{CSVParser, ColumnsFilter}
+import com.wojtechnology.sentiment.parse.CSVParser
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /** Main object for SentimentAnalysis tool */
@@ -12,10 +16,66 @@ object SentimentAnalysis {
     if (args.length < 1) {
       println("Did not provide a filename")
     } else {
-      val fileName = args.head
       val sc = initSpark("SentimentAnalysis")
+      val sqlContext = new SQLContext(sc)
 
-      countTokens(sc.textFile(fileName), ":(")
+      // Prepare training data from a list of (label, features) tuples.
+      val training = sqlContext.createDataFrame(Seq(
+        (1.0, Vectors.dense(0.0, 1.1, 0.1)),
+        (0.0, Vectors.dense(2.0, 1.0, -1.0)),
+        (0.0, Vectors.dense(2.0, 1.3, 1.0)),
+        (1.0, Vectors.dense(0.0, 1.2, -0.5))
+      )).toDF("label", "features")
+
+      // Create a LogisticRegression instance.  This instance is an Estimator.
+      val lr = new LogisticRegression()
+      // Print out the parameters, documentation, and any default values.
+      println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
+
+      // We may set parameters using setter methods.
+      lr.setMaxIter(10)
+        .setRegParam(0.01)
+
+      // Learn a LogisticRegression model.  This uses the parameters stored in lr.
+      val model1 = lr.fit(training)
+      // Since model1 is a Model (i.e., a Transformer produced by an Estimator),
+      // we can view the parameters it used during fit().
+      // This prints the parameter (name: value) pairs, where names are unique IDs for this
+      // LogisticRegression instance.
+      println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
+
+      // We may alternatively specify parameters using a ParamMap,
+      // which supports several methods for specifying parameters.
+      val paramMap = ParamMap(lr.maxIter -> 20)
+        .put(lr.maxIter, 30) // Specify 1 Param.  This overwrites the original maxIter.
+        .put(lr.regParam -> 0.1, lr.threshold -> 0.55) // Specify multiple Params.
+
+      // One can also combine ParamMaps.
+      val paramMap2 = ParamMap(lr.probabilityCol -> "myProbability") // Change output column name
+      val paramMapCombined = paramMap ++ paramMap2
+
+      // Now learn a new model using the paramMapCombined parameters.
+      // paramMapCombined overrides all parameters set earlier via lr.set* methods.
+      val model2 = lr.fit(training, paramMapCombined)
+      println("Model 2 was fit using parameters: " + model2.parent.extractParamMap)
+
+      // Prepare test data.
+      val test = sqlContext.createDataFrame(Seq(
+        (1.0, Vectors.dense(-1.0, 1.5, 1.3)),
+        (0.0, Vectors.dense(3.0, 2.0, -0.1)),
+        (1.0, Vectors.dense(0.0, 2.2, -1.5))
+      )).toDF("label", "features")
+
+      // Make predictions on test data using the Transformer.transform() method.
+      // LogisticRegression.transform will only use the 'features' column.
+      // Note that model2.transform() outputs a 'myProbability' column instead of the usual
+      // 'probability' column since we renamed the lr.probabilityCol parameter previously.
+      model2.transform(test)
+        .select("features", "label", "myProbability", "prediction")
+        .collect()
+        .foreach { case Row(features: Vector, label: Double, prob: Vector, prediction: Double) =>
+          println(s"($features, $label) -> prob=$prob, prediction=$prediction")
+        }
     }
   }
 
@@ -36,14 +96,13 @@ object SentimentAnalysis {
     * @return Unit
     */
   def countTokens(rawData: RDD[String], token: String) = {
-    val parser = new CSVParser with ColumnsFilter
+    val parser = new CSVParser
 
     val parsed = parser.parse(rawData)
-    val filtered = parser.filter(parsed, Seq(5))
-    val hasSmiley = filtered.filter(_.head.contains(token))
+    val hasSmiley = parsed.filter(_(6).contains(token))
     val countSmiley = hasSmiley.count()
 
-    println(s"Found $countSmiley smileys!")
+    println(s"Found $countSmiley '$token's")
     hasSmiley.foreach(println)
   }
 }
