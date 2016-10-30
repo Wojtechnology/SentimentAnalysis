eac28b3f8075c5eddb237c6e98a7459ba3994412
Wojtek Swiderski <wswiders@uwaterloo.ca>
2016-10-30 01:18:55 -0700

    Naive Bayes TFIDF simple model

--- build.sbt
+++ build.sbt
@@ -6,8 +6,8 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "org.scalatest" % "scalatest_2.11" % "3.0.0" % "test",
-  "org.apache.spark" %% "spark-core" % "2.0.0",
-  "org.apache.spark" %% "spark-mllib" % "2.0.0",
-  "com.holdenkarau" %% "spark-testing-base" % "2.0.0_0.4.4" % "test",
+  "org.apache.spark" %% "spark-core" % "2.0.1",
+  "org.apache.spark" %% "spark-mllib" % "2.0.1",
+  "com.holdenkarau" %% "spark-testing-base" % "2.0.1_0.4.7" % "test",
   "com.databricks" % "spark-csv_2.11" % "1.4.0"
 )
--- src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
+++ src/main/scala/com/wojtechnology/sentiment/SentimentAnalysis.scala
@@ -1,14 +1,12 @@
 package com.wojtechnology.sentiment
 
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel, Tokenizer}
-import org.apache.spark.ml.param.ParamMap
-import org.apache.spark.ml.linalg.{Vector, Vectors}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.functions._
+import org.apache.log4j.LogManager
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.feature._
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 
 
 case class RankedTweet(polarity: Int,
@@ -20,109 +18,108 @@ case class RankedTweet(polarity: Int,
 
 /** Main object for SentimentAnalysis tool */
 object SentimentAnalysis {
+  val log = LogManager.getRootLogger
 
   /** Main function for program */
   def main(args: Array[String]) {
-    if (args.length < 1) {
-      println("Did not provide a filename")
+    if (args.length < 2) {
+      log.error("Did not provide a train and test data")
     } else {
       val spark = initSpark("SentimentAnalysis")
+      val trainPath = args(0)
+      val testPath = args(1)
 
       import spark.implicits._
 
-      val schema = StructType(Seq(
-        StructField("polarity", IntegerType, nullable = false),
-        StructField("id", LongType, nullable = false),
-        StructField("dateString", StringType, nullable = false),
-        StructField("query", StringType, nullable = false),
-        StructField("user", StringType, nullable = false),
-        StructField("text", StringType, nullable = false)
-      ))
+      val productLabels = (polarity: Double) => if (polarity == 4.0) 1.0 else 0.0
+      val udfProductLabels = udf(productLabels).apply(col("polarity"))
 
-      val df = spark.sqlContext.read
-        .format("com.databricks.spark.csv")
-        .schema(schema)
-        .load(args(0))
+      val trainDf = readCSV(spark, trainPath)
+      val trainDfTokenized = tokenize(trainDf)
+      val trainDfCleaned = clean(trainDfTokenized)
 
-      val tokenizer: Tokenizer = new Tokenizer()
-        .setInputCol("text")
-        .setOutputCol("tokens")
+      val tfidfPipeline = tfidfModel(trainDfCleaned)
+      val trainDfTransformed = tfidfPipeline
+        .transform(trainDfCleaned)
+        .withColumn("label", udfProductLabels)
 
-      val countVectorizer: CountVectorizer = new CountVectorizer()
-        .setInputCol("tokens")
-        .setOutputCol("features")
+      val clf = new NaiveBayes()
+        .setFeaturesCol("features")
+        .setLabelCol("label")
+        .setPredictionCol("prediction")
+        .fit(trainDfTransformed)
 
-      val pipeline = new Pipeline()
-        .setStages(Array(tokenizer, countVectorizer))
+      val testDf = readCSV(spark, testPath).filter(row => row.getDouble(0) != 2.0)
 
-      val cvModel = pipeline.fit(df)
+      val testDfTokenized = tokenize(testDf)
+      val testDfCleaned = clean(testDfTokenized)
+      val testDfTransformed = tfidfPipeline
+        .transform(testDfCleaned)
+        .withColumn("label", udfProductLabels)
 
-      val featuredData = cvModel.transform(df)
+      val predictions = clf.transform(testDfTransformed)
 
-      featuredData.show()
+      val testCount = testDf.count
+      val correctCount = predictions.select("label", "prediction").filter(row => {
+        row.getDouble(0) == row.getDouble(1)
+      }).count
+      val correct = correctCount.toFloat / testCount.toFloat
+      println(s"Correct: $correct, $correctCount / $testCount")
     }
   }
 
-  def mlLibExample() = {
-    val spark = initSpark("SentimentAnalysis")
+  def tokenize(df: DataFrame): DataFrame = {
+    val tokenizer = new Tokenizer().setInputCol("rawText").setOutputCol("tokens")
+    tokenizer.transform(df)
+  }
 
-    // Prepare training data from a list of (label, features) tuples.
-    val training = spark.createDataFrame(Seq(
-      (1.0, Vectors.dense(0.0, 1.1, 0.1)),
-      (0.0, Vectors.dense(2.0, 1.0, -1.0)),
-      (0.0, Vectors.dense(2.0, 1.3, 1.0)),
-      (1.0, Vectors.dense(0.0, 1.2, -0.5))
-    )).toDF("label", "features")
+  /**
+    * Trains a tfidf vectorizer model and returns it
+    *
+    * @param df DataFrame containing column "cleanTokens"
+    * @return tfidf pipeline model
+    */
+  def tfidfModel(df: DataFrame): PipelineModel = {
+    val countVectorizer = new CountVectorizer()
+      .setInputCol("tokensClean")
+      .setOutputCol("featuresRaw")
+    val idf = new IDF().setInputCol("featuresRaw").setOutputCol("features")
+    val pipeline = new Pipeline().setStages(Array(countVectorizer, idf))
+    pipeline.fit(df)
+  }
 
-    // Create a LogisticRegression instance. This instance is an Estimator.
-    val lr = new LogisticRegression()
-    // Print out the parameters, documentation, and any default values.
-    println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
+  /**
+    * Cleans dataframe "tokens" column by removing stop words
+    *
+    * @param df
+    * @return
+    */
+  def clean(df: DataFrame): DataFrame = {
+    val remover = new StopWordsRemover().setInputCol("tokens").setOutputCol("tokensClean")
+    remover.transform(df)
+  }
 
-    // We may set parameters using setter methods.
-    lr.setMaxIter(10)
-      .setRegParam(0.01)
+  /**
+    * Reads CSV into dataframe
+    *
+    * @param spark Spark Session
+    * @param path CSV path
+    * @return DataFrame containing data in CSV
+    */
+  def readCSV(spark: SparkSession, path: String): DataFrame = {
+    val schema = StructType(Seq(
+      StructField("polarity", DoubleType, nullable = false),
+      StructField("id", LongType, nullable = false),
+      StructField("dateString", StringType, nullable = false),
+      StructField("query", StringType, nullable = false),
+      StructField("user", StringType, nullable = false),
+      StructField("rawText", StringType, nullable = false)
+    ))
 
-    // Learn a LogisticRegression model. This uses the parameters stored in lr.
-    val model1 = lr.fit(training)
-    // Since model1 is a Model (i.e., a Transformer produced by an Estimator),
-    // we can view the parameters it used during fit().
-    // This prints the parameter (name: value) pairs, where names are unique IDs for this
-    // LogisticRegression instance.
-    println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
-
-    // We may alternatively specify parameters using a ParamMap,
-    // which supports several methods for specifying parameters.
-    val paramMap = ParamMap(lr.maxIter -> 20)
-      .put(lr.maxIter, 30)  // Specify 1 Param. This overwrites the original maxIter.
-      .put(lr.regParam -> 0.1, lr.threshold -> 0.55)  // Specify multiple Params.
-
-    // One can also combine ParamMaps.
-    val paramMap2 = ParamMap(lr.probabilityCol -> "myProbability")  // Change output column name.
-    val paramMapCombined = paramMap ++ paramMap2
-
-    // Now learn a new model using the paramMapCombined parameters.
-    // paramMapCombined overrides all parameters set earlier via lr.set* methods.
-    val model2 = lr.fit(training, paramMapCombined)
-    println("Model 2 was fit using parameters: " + model2.parent.extractParamMap)
-
-    // Prepare test data.
-    val test = spark.createDataFrame(Seq(
-      (1.0, Vectors.dense(-1.0, 1.5, 1.3)),
-      (0.0, Vectors.dense(3.0, 2.0, -0.1)),
-      (1.0, Vectors.dense(0.0, 2.2, -1.5))
-    )).toDF("label", "features")
-
-    // Make predictions on test data using the Transformer.transform() method.
-    // LogisticRegression.transform will only use the 'features' column.
-    // Note that model2.transform() outputs a 'myProbability' column instead of the usual
-    // 'probability' column since we renamed the lr.probabilityCol parameter previously.
-    model2.transform(test)
-      .select("features", "label", "myProbability", "prediction")
-      .collect()
-      .foreach { case Row(features: Vector, label: Double, prob: Vector, prediction: Double) =>
-        println(s"($features, $label) -> prob=$prob, prediction=$prediction")
-      }
+    spark.sqlContext.read
+      .format("com.databricks.spark.csv")
+      .schema(schema)
+      .load(path)
   }
 
   /**
